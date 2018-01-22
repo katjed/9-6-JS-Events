72e8cae9c6c63c73beb6a48cf7a9e26f70e446fb
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-22 04:03:39 +0100

    Update index.html, update js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
-
 	var element = document.createElement('li');
 	var elementCount = document.getElementsByTagName('li').length;
 	element.innerHTML = 'item ' + elementCount;
-	list.appendChild(element);
+	list.appendChild(element);	
 });
 
 
